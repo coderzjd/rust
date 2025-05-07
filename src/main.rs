47678564b2293e4262fcd33fe7b2f07bc4439161
coderzjd <1633107710@qq.com>
2025-05-07 17:59:11 +0800

    21

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-fn main() {
-    // Sync and Send Traits
-    // Send marker 特征指示实施 Send 类型的值的所有权可以在线程之间转移。几乎所有 Rust 类型都是 Send
-    // 完全由 Send 类型组成的任何类型也会自动标记为 Send。
+use trpl::Html;
 
-    // Sync marker trait 表示它对于实现 sync 以从多个线程引用。换句话说，任何类型 T 都是 Sync if &T （对 T 的不可变引用） 为 Send，
-    // 这意味着该引用可以安全地发送到另一个线程。与 Send 类似，原始类型是 Sync 和完全由 Sync 类型组成的类型也是 Sync。
+fn main() {
+    trpl::run(async {
+        let title = page_title("https://www.baidu_dasdhasd.com/").await;
+        match title {
+            Some(info) => println!("info is {info}"),
+            None => println!("no title"),
+        }
+    })
+}
+async fn page_title(url: &str) -> Option<String> {
+    let rsp = trpl::get(url).await;
+    let rsp_text = rsp.text().await;
+    Html::parse(&rsp_text)
+        .select_first("title")
+        .map(|title_element| title_element.inner_html())
 }
