cefe8d0e838168addbc091776423fd53781c8d1c
coderzjd <1633017710@qq.com>
2025-05-09 23:23:28 +0800

    27

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,27 @@ fn main() {
     // 1、Rust允许多个Trait拥有相同方法名称
     // 2、也可以在同一个类型上实现多个带有相同方法名称的Trait
     // 3、类型本身也可以定义相同名称的方法
-    struct Foo {}
-    struct Bar {}
-    impl Foo {
-        fn run() {}
+
+    trait Foo {
+        fn run(&self) {
+            println!("trait Foo 的 run 方法")
+        }
     }
-    impl Bar {
-        fn run() {}
+    trait Bar {
+        fn run(&self) {
+            println!("trait Bar 的 run 方法")
+        }
     }
+    struct Gkd {}
+    impl Gkd {
+        fn run(&self) {
+            println!("Gkd 自己实现的 run 方法")
+        }
+    }
+    impl Foo for Gkd {}
+    impl Bar for Gkd {}
+    let g1 = Gkd {};
+    g1.run();
+    Foo::run(&g1);
+    Bar::run(&g1);
 }
