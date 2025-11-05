245f9bbc885a35b81616d4cac24e0f6872c5a14a
coderzjd <1633107710@qq.com>
2025-11-05 11:38:49 +0800

    34

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
 fn main() {
-   trpl::run(async {
-        let body = reqwest::get("https://www.rust-lang.org")
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
-
-        println!("body = {body:?}");
-    })
+   println!("hello rust")
 }
